7c954b7df47eb89e90ec9c192d597ca71183b9d5
unknown <C:\Users\\AppData\Roaming\The Bat!>
2018-09-10 01:10:09 +0300

    UI is ready

--- Assets/Scripts/ScreenSelected.cs
+++ Assets/Scripts/ScreenSelected.cs
@@ -16,7 +16,9 @@ public class ScreenSelected : MonoBehaviour {
     private static Color choosed = new Color(1, 1, 1, 1);
     private static Color unChoosed = new Color(1, 1, 1, 0.5f);
     private static Vector3 moove = new Vector3(1080, 0, 0);
-    private const int speed = 1000;
+    private const int speed = 5;
+    private bool isStaying = true;
+    private bool off = true;
     private Vector3 rightPos;
 
     void Start () {
@@ -27,15 +29,29 @@ public class ScreenSelected : MonoBehaviour {
     {
         if (ShownMenu != null)
         {
-            if (ShownMenu.transform.localPosition != rightPos)
+            if (Menu.transform.localPosition != rightPos)
             {
-                Menu.transform.localPosition = Vector3.MoveTowards(Menu.transform.localPosition, rightPos, Time.deltaTime * speed);
+                float distance = (Menu.transform.localPosition - rightPos).magnitude + 100;
+                Menu.transform.localPosition = Vector3.MoveTowards(Menu.transform.localPosition, rightPos, Time.deltaTime * speed * distance);
             }
+            else
+                isStaying = true;            
+        }
+
+        if (isStaying && off)
+        {
+            for (int i = 0; i < menuPair.Length; i++)
+                menuPair[i].menu.SetActive(false);
+
+            menuPair[numOfMenu].menu.SetActive(true);
+            off = false;
         }
     }
 
     public void Choose()
     {
+        isStaying = false;
+        off = true;
         choosedMenu = EventSystem.current.currentSelectedGameObject;
 
         foreach (var i in menuPair)
