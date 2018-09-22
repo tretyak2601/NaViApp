4a3c25beb62063554516e3f349a90bd17f404893
unknown <C:\Users\\AppData\Roaming\The Bat!>
2018-09-23 01:51:30 +0300

    micro changes

--- Assets/Scripts/UI/ScreenSelected.cs
+++ Assets/Scripts/UI/ScreenSelected.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class ScreenSelected : MonoBehaviour {
     
@@ -10,6 +8,7 @@ public class ScreenSelected : MonoBehaviour {
     [SerializeField] Image lastGamesButtonImage;
     [SerializeField] Button[] buttons;
     [SerializeField] MenuObj[] menus;
+    [SerializeField] GameObject[] Tables;
 
     public static Color choosed = new Color(1f, 0.964f, 0.164f, 1);
     public static Color unChoosed = new Color(1, 1, 1, 0.5f);
@@ -29,6 +28,9 @@ public class ScreenSelected : MonoBehaviour {
 
     IEnumerator Mooving(MenuObj obj)
     {
+        for (int i = 0; i < Tables.Length; i++)
+            Tables[i].SetActive(true);
+
         while (true)
         {
             if (Menu.transform.localPosition != obj.menuPosition)
@@ -60,6 +62,12 @@ public class ScreenSelected : MonoBehaviour {
                 temp.bottomImage.gameObject.SetActive(false);
             }
         }
+
+        for (int i = 0; i < Tables.Length; i++)
+        {
+            if (i != obj.menuNum)
+                Tables[i].SetActive(false);
+        }
     }
 
     public void SlideRightToLeft()
@@ -68,40 +76,28 @@ public class ScreenSelected : MonoBehaviour {
         Menu.transform.localPosition = new Vector3(tapPos, Menu.transform.localPosition.y, Menu.transform.localPosition.z) / 10f;
     }
 
-    public void OnEndDragRight()
-    {
-        if (Menu.transform.localPosition.x - menuPos.x < -50f && numMenu < 2)
-        {
-            numMenu++;
-            MenuObj obj = menus[numMenu];
-            StartCoroutine(Mooving(obj));
-        }
-        else
-        {
-            MenuObj obj = menus[numMenu];
-            StartCoroutine(Mooving(obj));
-        }
-    }
-
     public void SlideLeftToRight()
     {
         float tapPos = Input.mousePosition.x + menuPos.x * 10;
         Menu.transform.localPosition = new Vector3(tapPos, Menu.transform.localPosition.y, Menu.transform.localPosition.z) / 10f;
     }
 
+    public void OnEndDragRight()
+    {
+        if (Menu.transform.localPosition.x - menuPos.x < -50f && numMenu < 2)
+            numMenu++;
+
+        MenuObj obj = menus[numMenu];
+        StartCoroutine(Mooving(obj));
+    }
+
     public void OnEndDragLeft()
     {
         if (Menu.transform.localPosition.x > menuPos.x + 50f && numMenu > 0)
-        {
             numMenu--;
-            MenuObj obj = menus[numMenu];
-            StartCoroutine(Mooving(obj));
-        }
-        else
-        {
-            MenuObj obj = menus[numMenu];
-            StartCoroutine(Mooving(obj));
-        }
+
+        MenuObj obj = menus[numMenu];
+        StartCoroutine(Mooving(obj));
     }
 
     public void SetColor()
