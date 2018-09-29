95892031113fd7ff8f76f50d797cbd24582b8f2e
unknown <C:\Users\\AppData\Roaming\The Bat!>
2018-09-29 22:12:30 +0300

    Better scroll and sliding

--- Assets/Scripts/UI/ScreenSelected.cs
+++ Assets/Scripts/UI/ScreenSelected.cs
@@ -1,28 +1,34 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ScreenSelected : MonoBehaviour {
-    
+public class ScreenSelected : MonoBehaviour
+{
+
     [SerializeField] GameObject Menu;
     [SerializeField] GameObject leftMenu;
     [SerializeField] Image lastGamesButtonImage;
     [SerializeField] Button[] buttons;
     [SerializeField] MenuObj[] menus;
     [SerializeField] GameObject[] Tables;
+    [SerializeField] ScrollRect currentScroll;
 
     public static Color choosed = new Color(1f, 0.964f, 0.164f, 1);
     public static Color unChoosed = new Color(1, 1, 1, 1f);
+    private Vector3 tapPos;
+    public float deltaPos;
 
     private const string greyHexColor = "#717686";
 
     private static int numMenu = 2;
     private static Vector3 menuPos = Vector3.zero;
 
-    void Start () {
+    void Start()
+    {
         MenuObj.OnMenuChanged += MoveMenu;
         ColorUtility.TryParseHtmlString(greyHexColor, out unChoosed);
-	}
+    }
 
     private void MoveMenu(Button obj)
     {
@@ -45,8 +51,9 @@ public class ScreenSelected : MonoBehaviour {
             {
                 if (obj.menuNum == 2)
                     leftMenu.SetActive(true);
-
+                
                 numMenu = obj.menuNum;
+                currentScroll = Tables[numMenu].transform.GetComponentInChildren<ScrollRect>();
                 menuPos = Menu.transform.localPosition;
                 OffOthersButton(obj);
                 obj.mainImage.color = choosed;
@@ -79,34 +86,54 @@ public class ScreenSelected : MonoBehaviour {
         }
     }
 
-    public void SlideRightToLeft()
+    public void MouseUp()
     {
-        float tapPos = Input.mousePosition.x - 1080 + menuPos.x * 5;
-        Menu.transform.localPosition = new Vector3(tapPos, Menu.transform.localPosition.y, Menu.transform.localPosition.z) / 5f;
+        currentScroll.vertical = true;
+        tapPos = Vector3.zero;
+        deltaPos = 0;
     }
 
-    public void SlideLeftToRight()
+    public void MouseDown()
     {
-        leftMenu.SetActive(false);
-        float tapPos = Input.mousePosition.x + menuPos.x * 5;
-        Menu.transform.localPosition = new Vector3(tapPos, Menu.transform.localPosition.y, Menu.transform.localPosition.z) / 5f;
+        tapPos = Input.mousePosition;
+    }
+
+    float oldDelta = 0;
+
+    private void Update()
+    {
+        if (oldDelta == deltaPos)
+        {
+            deltaPos = 0;
+            tapPos = Input.mousePosition;
+            return;
+        }
+
+        oldDelta = deltaPos;
+    }
+
+    public void SlideRightToLeft()
+    {
+        if (tapPos != Vector3.zero)
+        {
+            deltaPos = tapPos.x - Input.mousePosition.x;
+
+            if (Mathf.Abs(deltaPos) < 200) 
+                return;
+
+            currentScroll.vertical = false;
+            Menu.transform.localPosition = new Vector3(Menu.transform.localPosition.x - deltaPos / 50, Menu.transform.localPosition.y, Menu.transform.localPosition.z);
+        }
     }
 
     public void OnEndDragRight()
     {
         if (Menu.transform.localPosition.x - menuPos.x < -35f && numMenu < menus.Length - 1)
             numMenu++;
+        else if (Menu.transform.localPosition.x > menuPos.x + 35f && numMenu > 0)
+            numMenu--;
         else
             leftMenu.SetActive(true);
-
-        MenuObj obj = menus[numMenu];
-        StartCoroutine(Mooving(obj));
-    }
-
-    public void OnEndDragLeft()
-    {
-        if (Menu.transform.localPosition.x > menuPos.x + 35f && numMenu > 0)
-            numMenu--;
 
         MenuObj obj = menus[numMenu];
         StartCoroutine(Mooving(obj));
